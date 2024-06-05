f344071dc92cda7b590cc50039d5c80f85da606d
chiaracostantino <costantinchia@gmail.com>
2024-06-05 15:00:31 +0200

    ho aggiornato la mia porzione di lavoro

---
+++ src/main/java/gui/LoginCliente.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.*;
+
+public class loginCliente {
+    private JPanel panel1;
+    private JLabel REGISTRAZIONE;
+    private JButton eMailButton;
+    private JPasswordField passwordField1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
