172112a1f192153196e6816bfcb079e6b45834fd
chiaracostantino <costantinchia@gmail.com>
2024-06-05 16:32:42 +0200

    HomeLavoratore terminato

---
+++ src/main/java/gui/LoginLavoratore/LoginLavoratore.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.*;
+
+public class LoginLavoratore {
+        private JPanel panel1;
+        private JLabel REGISTRAZIONE;
+        private JCheckBox mostraPassword;
+        private JTextField textField1;
+        private JButton registratiButton;
+        private JCheckBox mostraPasswordCheckBox;
+
+
+      public LoginLavoratore() {
+      }
+}
