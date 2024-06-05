af2c2284ae283a165566d53a04a400219e475f23
chiaracostantino <costantinchia@gmail.com>
2024-06-05 17:39:47 +0200

    HomeLavoratore terminato

--- src/main/java/gui/LoginCliente.java
+++ src/main/java/gui/LoginCliente.java
@@ -1,14 +1,13 @@
 package gui;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class LoginCliente {
     private JPanel panel1;
     private JLabel REGISTRAZIONE;
     private JCheckBox mostraPassword;
     private JTextField textField1;
+    private JPasswordField passwordField1;
     private JButton registratiButton;
     private JCheckBox mostraPasswordCheckBox;
 
