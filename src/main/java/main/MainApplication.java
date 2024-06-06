687c97dc70a4557caac102a48166dd2f9596cb05
andrea <andreafarfaglia2002@gmail.com>
2024-06-06 18:22:01 +0200

    da errore

--- src/main/java/main/MainApplication.java
+++ src/main/java/main/MainApplication.java
@@ -1,5 +1,6 @@
 package main;
 
+import gui.House;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+        //new House();
     }
 }
